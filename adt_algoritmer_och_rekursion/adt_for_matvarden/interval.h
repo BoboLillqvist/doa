682e857eb829b248d3e32d2bef3e3e17b550d438
BoboLillqvist <bobo.lillqvist@gmail.com>
2017-08-29 11:07:13 +0100

    1.1 Lägg till metodsdefinitioner för klassen

--- adt_algoritmer_och_rekursion/adt_for_matvarden/interval.h
+++ adt_algoritmer_och_rekursion/adt_for_matvarden/interval.h
@@ -19,4 +19,30 @@ public:
 		lower = (T)(mean - mean * procent / 100);
 		upper = (T)(mean + mean * procent / 100);
 	}
+	friend interval<T> operator+(const interval<T> &lop, const interval<T> &rop)
+	{
+
+	}
+	friend interval<T> operator-(const interval<T> &lop, const interval<T> &rop)
+	{
+
+	}
+	friend interval<T> operator*(const interval<T> &lop, const interval<T> &rop)
+	{
+
+	}
+	double absoluteError()
+	{
+
+	}
+
+	double relativeError()
+	{
+
+	}
+
+	string print()
+	{
+
+	}
 };
