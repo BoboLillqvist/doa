120d3059fabd1da0556dc688f9e646700069e962
Tobias Lans <lanstobias@gmail.com>
2017-09-03 13:31:19 +0200

    1.3 Lägg till labben "Rekursiv triangleutskrift"

---
+++ adt_algoritmer_och_rekursion/rekursiv_triangelutskrift/main.cpp
@@ -0,0 +1,62 @@
+//
+// Labb 3: Rekursiv triangelutskrift
+// Örebro Universitet
+// Tobias Lans, Bobo Lillqvist
+// https://github.com/BoboLillqvist/doa
+//
+#include <iostream>
+#include <sstream>  // ostream
+using namespace std;
+
+void triangle(ostream& os, size_t m, size_t n);
+string lineOfNChars(char c, int n);
+
+// Main app
+void app()
+{
+    size_t m, n;
+
+    cout << "Recursive triangle!" << endl << endl;
+    cout << "Enter min size: "; cin >> m;
+    cout << "Enter mind size: "; cin >> n;
+    cout << endl;
+
+    triangle(cout, m, n);
+    cout << endl;
+}
+
+int main(void)
+{
+    app();
+    system("PAUSE");
+}
+
+// Print a triangle of chars recursively
+void triangle(ostream& os, size_t m, size_t n)
+{
+    if ((m - 1) == n)
+        return;
+
+    // Print m stars
+    os << lineOfNChars('*', m);
+
+        // Recursive part
+    if (m <= n)
+    {
+        triangle(os, (m + 1), n);
+        os << lineOfNChars('*', m);
+    }
+}
+
+// Returns n number of chars as a string
+string lineOfNChars(char c, int n)
+{
+    ostringstream oss;
+    for (int i = 0; i < n; i++)
+    {
+        oss << "*";
+    }
+    oss << endl;
+
+    return oss.str();
+}
