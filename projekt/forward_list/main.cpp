e29b1bb4cefd95380513ecf6df34ad18718fbd60
Tobias Lans <lanstobias@gmail.com>
2017-09-07 10:55:42 +0200

    P1 Skapa projekt i Visual Studio

---
+++ projekt/forward_list/main.cpp
@@ -0,0 +1,19 @@
+//
+// https://github.com/BoboLillqvist/doa
+//
+// Datastrukturer och algoritmer
+// Projekt: Testdriven utveckling av enkellänkade listan forward_list<T>
+//
+// Tobias Lans och Bobo Lillqvist
+// Örebro Universitet
+// HT2017
+//
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+
+    system("PAUSE");
+    return 0;
+}
