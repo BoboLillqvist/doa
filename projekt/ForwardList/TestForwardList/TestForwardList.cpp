ba20bf13103fa4e0e998e85c606c3af84e804104
Tobias Lans <lanstobias@gmail.com>
2017-09-08 12:08:18 +0200

    P1 Skapa testklass för Node

--- projekt/ForwardList/TestForwardList/TestForwardList.cpp
+++ projekt/ForwardList/TestForwardList/TestForwardList.cpp
@@ -31,4 +31,26 @@ namespace TestForwardList
 			ForwardList<int> fli;
 		}
 	};
+
+    TEST_CLASS(TestNode)
+    {
+    public:
+
+        TEST_METHOD(TestCreateEmptyNode)
+        {
+            Node<int> node1;
+            Assert::IsTrue(node1.next == nullptr);
+        }
+
+        TEST_METHOD(TestCreateNodeWithValue)
+        {
+            Node<int> node1(5);
+            Node<int> node2(10000000);
+            Node<double> node3(5.0003);
+
+            Assert::AreEqual(node1.value, 5);
+            Assert::AreEqual(node2.value, 10000000);
+            Assert::AreEqual(node3.value, 5.0003);
+        }
+    };
 }
