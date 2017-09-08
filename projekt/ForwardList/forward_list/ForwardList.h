9bf48f4cdda6e64833d5261737ba2baf4d1f06d3
BoboLillqvist <bobo.lillqvist@gmail.com>
2017-09-08 11:35:50 +0100

    P1 Lägg till ForwardListklassens skelett

--- projekt/ForwardList/forward_list/ForwardList.h
+++ projekt/ForwardList/forward_list/ForwardList.h
@@ -3,14 +3,85 @@ template <class T>
 class ForwardList
 {
 public:
+	//Constructors
     ForwardList()
     {
 
     }
+	//Destructor
+	~ForwardList()
+	{
 
-    ~ForwardList()
-    {
+	}
 
-    }
+	//Capacity
+	bool empty() 
+	{ 
+		return true; 
+	}
+
+	//Elementaccess
+	T& front()
+	{
+
+	}
+
+	//Modifiers
+	void pushFront(const T& value)
+	{
+
+	}
+
+	void popFront()
+	{
+
+	}
+ 
+	void clear()
+	{
+
+	}
+
+	void insertAfter(T& node, T& newNode)
+	{
+
+	}
+
+	void eraseAfter(T& node)
+	{
+		//Ta bort nodens next
+		//Kalla på next nodes destruktor?
+	}
+
+	void swap(ForwardList<T> listToSwap)
+	{
+		//Ändra den listan som kallar på funktionen med den listan som skickas in
+		//Kolla så att båda har samma <T>
+	}
+	//Iterator
+	class Iterator
+	{
+	public:
+		//Constructor
+		Iterator()
+		{
+
+		}
+		//Destructor
+		~Iterator()
+		{
+
+		}
+	private:
+		
+	};
+	Iterator begin()
+	{
+
+	}
+	Iterator end()
+	{
+
+	}
 };
 
