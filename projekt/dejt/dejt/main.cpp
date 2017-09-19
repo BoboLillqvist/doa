b9c34ad766ecce7dd72ac77f6338b913b32569a2
Tobias Lans <lanstobias@gmail.com>
2017-09-19 09:34:54 +0200

    P2 Lägg till kommentar i main

--- projekt/dejt/dejt/main.cpp
+++ projekt/dejt/dejt/main.cpp
@@ -1,3 +1,15 @@
+//
+// Dejt i C++
+// https://github.com/BoboLillqvist/doa
+//
+// Datastrukturer och algoritmer
+// Örebro Universitet
+// HT2017
+// 
+// Tobias Lans
+// Bobo Lillqivist
+//
+
 #include <iostream>
 using namespace std;
 
