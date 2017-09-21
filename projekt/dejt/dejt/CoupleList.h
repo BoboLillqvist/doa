68966d03730b94ae84111866c374f5426b6576f6
BoboLillqvist <bobo.lillqvist@gmail.com>
2017-09-21 10:25:01 +0100

    P2 Lägg till tom klass för CoupleList

---
+++ projekt/dejt/dejt/CoupleList.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <forward_list>
+#include "Couple.h"
+
+class CoupleList
+{
+private:
+	std::forward_list<Couple> list;
+public:
+	CoupleList()
+	{
+
+	}
+
+	std::forward_list<Couple>& getList()
+	{
+
+	}
+};
