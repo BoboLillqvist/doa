ba4cc4fdad103b25158c2bf7ccad6abc428178db
Tobias Lans <lanstobias@gmail.com>
2017-09-22 15:49:17 +0200

    P2 Skriv konstruktor för Person

--- projekt/dejt/dejt/Person.h
+++ projekt/dejt/dejt/Person.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "InterestTable.h"
+#include <vector>
+
 class Person
 {
 private:
@@ -10,8 +12,19 @@ private:
 	InterestTable it_;
 
 public:
-	Person()
+    Person(size_t id, std::string name, char gender, std::vector<char> preference, std::vector<std::string> interests)
 	{
+        id_ = id;
+        name_ = name;
+        preference_ = preference;
+        addToInterestTable(interests);
+    }
+
+    void addToInterestTable(std::vector<std::string> interests)
+    {
+        for (auto interest : interests)
+            it_.add(interest);
+    }
 
 	}
 };
