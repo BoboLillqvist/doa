48f8f40b38576dc59c0f64a02b9f3ab0ba7e0eb6
BoboLillqvist <bobo.lillqvist@gmail.com>
2017-09-22 10:50:34 +0100

    P2 Lägg till felkontrollering i add metoden

--- projekt/dejt/dejt/InterestTable.h
+++ projekt/dejt/dejt/InterestTable.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <forward_list>
-
+#include <algorithm>
+#include <string>
 class InterestTable
 {
 private:
@@ -9,11 +10,19 @@ private:
 public:
     InterestTable() {}
 
-    void add(std::string interest)
-    {
-        // TODO: Kolla om intresset redan finns i listan
-        // TODO: Kör en toLower() innan, och ev. skala whitespace
-        list.push_front(interest);
+	void add(std::string interest)
+	{
+		bool isInList = false;
+		// TODO: Kolla om intresset redan finns i listan
+		// TODO: Kör en toLower() innan, och ev. skala whitespace
+		std::transform(interest.begin(), interest.end(), interest.begin(), ::tolower);
+		for (auto thisInterest : list)
+		{
+			if (thisInterest==interest)
+				isInList = true;
+		}
+		if (!isInList && !(interest==""))
+			list.push_front(interest);
     }
 
     std::forward_list<std::string>& getList()
@@ -31,11 +40,10 @@ public:
             // Check if current interest is in the other persons interest table 
             for (auto otherInterest : other.getList())
             {
-                if (thisInterest.compare(otherInterest) == 0)
+                if (thisInterest == otherInterest)
                     commonCounter++;
             }
         }
-
         return commonCounter;
     }
 };
