ddd9008f139a3a8ed3e777d33173e07420051f57
Tobias Lans <lanstobias@gmail.com>
2017-09-26 09:03:53 +0200

    P2 Ändra från lista till string i TestPerson

--- projekt/dejt/Testdejt/TestPerson.cpp
+++ projekt/dejt/Testdejt/TestPerson.cpp
@@ -14,8 +14,8 @@ TEST_CASE("Test Person Class")
     interests2.push_back("Mat");
     interests2.push_back("sport");
 
-    Person person1(1, "Bob", 'm', { 'm', 'f' }, interests);
-    Person person2(2, "Jens", 'm', { 'm', 'f' }, interests2);
+    Person person1(1, "Bob", 'm', "mf", interests);
+    Person person2(2, "Jens", 'm', "mf", interests2);
 
     SECTION("Test person interest table")
     {
