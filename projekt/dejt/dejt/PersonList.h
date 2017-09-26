4c83054f3129d059567047f3cc8831347b8dd73f
Tobias Lans <lanstobias@gmail.com>
2017-09-26 09:01:26 +0200

    P2 Skapa PersonList::fileToList()

--- projekt/dejt/dejt/PersonList.h
+++ projekt/dejt/dejt/PersonList.h
@@ -1,5 +1,10 @@
 #pragma once
 #include <forward_list>
+#include <iostream>
+#include <fstream> // std::ifstream
+#include <sstream>
+#include <exception>
+#include <vector>
 #include "Person.h"
 
 class PersonList
@@ -15,8 +20,22 @@ public:
         return list;
     }
 
-    void fileToString(std::string fileName)
+    void fileToList(std::string filename)
     {
+        std::string line;
+        std::ifstream listFile;
 
+        // Open the file for out operations
+        listFile.open(filename, std::ios::out);
+
+        if (!listFile)
+            throw std::exception("Could not open file");
+
+        // Read line by line
+        while (std::getline(listFile, line))
+            list.push_front(personFromString(line));
+
+        listFile.close();
+    }
     }
 };
