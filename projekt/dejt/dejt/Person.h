daeaf9e3a5e1e96aa29c465ae49735dff8136769
Tobias Lans <lanstobias@gmail.com>
2017-09-27 12:28:35 +0200

    P2 Skapa getters för Person

--- projekt/dejt/dejt/Person.h
+++ projekt/dejt/dejt/Person.h
@@ -12,24 +12,62 @@ private:
 	InterestTable it_;
 
 public:
+    // Constructors
     Person() {}
 
     Person(size_t id, size_t age, std::string name, char gender, std::vector<std::string> preferences, std::vector<std::string> interests)
 	{
         id_ = id;
+        age_ = age;
         name_ = name;
         preferences_ = preferences;
         addToInterestTable(interests);
     }
 
+    // Methods
     void addToInterestTable(std::vector<std::string> interests)
     {
         for (auto interest : interests)
             it_.add(interest);
     }
 
+    // Getters
+    size_t getID()
+    {
+        return id_;
+    }
+
+    size_t getAge()
+    {
+        return age_;
+    }
+
+    std::string getName()
+    {
+        return name_;
+    }
+
+    char getGender()
+    {
+        return gender_;
+    }
+
+    std::vector<std::string> getPreferences()
+    {
+        return preferences_;
+    }
+
     InterestTable getInterestTable()
     {
         return it_;
     }
-};
+
+    size_t numberOfPreferences()
+    {
+        size_t counter = 0;
+        for (auto pref : preferences_)
+            counter++;
+
+        return counter;
+    }
+
