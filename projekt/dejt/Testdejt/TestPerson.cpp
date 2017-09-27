66aecfe00b4a0d2157c1e22f6d9e0ee1b0bd0f3d
Tobias Lans <lanstobias@gmail.com>
2017-09-27 12:31:57 +0200

    P2 Skriv tester för Person

--- projekt/dejt/Testdejt/TestPerson.cpp
+++ projekt/dejt/Testdejt/TestPerson.cpp
@@ -1,6 +1,8 @@
 #include "catch.hpp"
 #include "../dejt/Person.h"
 #include <vector>
+#include <sstream>
+#include <iostream>
 
 TEST_CASE("Test Person Class")
 {
@@ -19,6 +21,56 @@ TEST_CASE("Test Person Class")
 
     SECTION("Test person interest table")
     {
-        REQUIRE(person1.getInterestTable() == person2.getInterestTable());
+        REQUIRE(bob.getInterestTable() == maja.getInterestTable());
+    }
+
+    SECTION("Test >> operator overload")
+    {
+        Person bob;
+
+        // Simulate file
+        std::string line = "0;20;Bob;m;female,male;kaffe,sport,sprit\n";
+
+        // Add in string stream
+        std::istringstream iss(line);
+
+        // Add first line data to person
+        iss >> bob;
+
+        SECTION("Test static length data")
+        {
+            // Test if static data was stored
+            REQUIRE(bob.getID() == 0);
+            REQUIRE(bob.getAge() == 20);
+            REQUIRE(bob.getName() == "Bob");
+            REQUIRE(bob.getGender() == 'm');
+        }
+
+        SECTION("Test preferences was stored", "[preferences]")
+        {
+            CHECK(bob.numberOfPreferences() == 2);
+            std::string bobPreference[] = { "female", "male" };
+
+            size_t i = 0;
+            for (auto pref : bob.getPreferences())
+            {
+                REQUIRE(pref == bobPreference[i]);
+                i++;
+            }
+        }
+
+        SECTION("Test if interests was stored", "[interests]")
+        {
+            CHECK(bob.getInterestTable().numberOfInterests() == 3);
+            std::string personInterests[] = { "kaffe", "sport", "sprit" };
+
+            size_t i = 0;
+            for (auto interest : bob.getInterestTable().getList())
+            {
+                REQUIRE(interest == personInterests[i]);
+                i++;
+            }
+        }
     }
 }
+
