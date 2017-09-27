7811601b933d5a0158cf46e4ac3100ac8a040a1f
Tobias Lans <lanstobias@gmail.com>
2017-09-27 12:32:19 +0200

    P2 Skriv tester för PersonList

--- projekt/dejt/Testdejt/TestPersonList.cpp
+++ projekt/dejt/Testdejt/TestPersonList.cpp
@@ -3,10 +3,23 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include <cstdio> // std::remove()
 using namespace std;
 
 TEST_CASE("Test List Class")
 {
+    SECTION("Test numPersonss() method")
+    {
+        Person bob(0, 23, "Bob", 'm', { "male", "female" }, {"sport", "fiske"});
+        Person maja(1, 30, "Maja", 'f', { "male" }, {"film", "musik"});
+
+        PersonList persons;
+        persons.getList().push_front(bob);
+        persons.getList().push_front(maja);
+
+        REQUIRE(persons.numPersons() == 2);
+    }
+
     SECTION("Test reading persons from file")
     {
         PersonList personList;
@@ -14,22 +27,17 @@ TEST_CASE("Test List Class")
         // TODO: Gör testet på en testfil som man skapar här
         SECTION("Test opening files")
         {
-            // Should throw exception
+            // Trying to open wrong file path should throw exception
             REQUIRE_THROWS(personList.fileToList("wrongfilename.csv"));
             REQUIRE_THROWS(personList.fileToList(""));
 
             // Should NOT throw exception
             REQUIRE_NOTHROW(personList.fileToList("../dejt/male.csv"));
 
-            SECTION("Test numPersons() method")
+            SECTION("Test if right number of persons was stored")
             {
                 REQUIRE(personList.numPersons() == 4);
             }
-        }
-
-        SECTION("Test the list")
-        {
-            // TODO: Write test for list
         }
     }
 }
