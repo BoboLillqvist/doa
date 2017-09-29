c0539f795a12ece389006683cfd3ca34c50cab3a
BoboLillqvist <bobo.lillqvist@gmail.com>
2017-09-29 10:50:32 +0100

    P2 Lägg till innehållet i Menu

--- projekt/dejt/dejt/Menu.h
+++ projekt/dejt/dejt/Menu.h
@@ -2,7 +2,6 @@
 #include "Date.h"
 
 //Kallar på date metoderna
-//Menu körs i main där en menu skapas och menySnurran kallas på automatiskt.
 class Menu
 {
 private:
@@ -10,50 +9,99 @@ private:
 public:
 	Menu()
 	{
-		menySnurra();
+
 	}
 
 	void menySnurra()
 	{
-		//While
-		display();
-		//Switch
-		//Case 1: Add new girl
-		//Case 2: Add new boy
-		//Case 3: Save to file
-		//Case 4: Read from file
-		//Case 5: Match
-		// Case 6: Quit
+		bool quitCheck = false;
+		while (!quitCheck)
+		{
+			display();
+			int switchCase = 0;
+			std::cin >> switchCase;
+			switch(switchCase)
+			{
+			case 1: 
+				newGirl();
+				break;
+			case 2:
+				newBoy();
+				break;
+			case 3:
+				saveToFile();
+				break;
+			case 4: 
+				readFromFile();
+				break;
+			case 5:
+				match();
+				break;
+			case 6: 
+				quitCheck = true;
+				break;
+			default:
+				system("pause");
+				break;
 
+			}
+			//Switch
+			//Case 1: Add new girl
+			//Case 2: Add new boy
+			//Case 3: Save to file
+			//Case 4: Read from file
+			//Case 5: Match
+			// Case 6: Quit
+		}
 	}
 	void display()
 	{
+		system("clear");
+		std::cout << "Main menu:"<<std::endl;
+		std::cout << "---------" << std::endl;
+		std::cout << "1. Add new girl" << std::endl;
+		std::cout << "2. Add new boy" << std::endl;
+		std::cout << "3. Save to file" << std::endl;
+		std::cout << "4. Read from file" << std::endl;
+		std::cout << "5. Match couples" << std::endl;
+		std::cout << "6. Quit" << std::endl;
+		
 		//Vad som kommer upp på skärmen varje gång menysnurra körs
 	}
 	void newGirl()
 	{
+		std::cout << std::endl<<"Girl added!" << std::endl;
+		system("pause");
 		//New person
 		//Add person to date.girllist
 	}
 
 	void newBoy()
 	{
+		std::cout << std::endl<<"Boy added!" << std::endl;
+		system("pause");
 		//New person
 		//Add person to date.girllist
 	}
 
 	void saveToFile()
 	{
+		std::cout << std::endl<< "File saved!" << std::endl;
+		system("pause");
 		//Kalla på personlist och eventuellt couplelist save metoder
 	}
 
 	void readFromFile()
 	{
+		std::cout << std::endl<< "File read!" << std::endl;
+		system("pause");
 		//Kalla på personlist och eventuellt couplelist read metoder
 	}
 
 	void match()
 	{
 		date.match();
+		std::cout << std::endl<< "Couples matched!" << std::endl;
+		system("pause");
 	}
 };
