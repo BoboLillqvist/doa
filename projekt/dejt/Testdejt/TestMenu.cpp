625fc8f1be40887cf468dc5bffb22276fec564bc
BoboLillqvist <bobo.lillqvist@gmail.com>
2017-09-29 14:14:35 +0100

    P2 Lägg till Test för menu.infoAboutPerson()

--- projekt/dejt/Testdejt/TestMenu.cpp
+++ projekt/dejt/Testdejt/TestMenu.cpp
@@ -7,5 +7,22 @@
 TEST_CASE("Test Menu class")
 {
 	Menu menu;
-	menu.menySnurra();
+	//menu.menySnurra();
+	std::vector<std::string> interests3;
+	interests3.push_back("guld");
+	interests3.push_back("pengar");
+	interests3.push_back("promenader");
+	Person person3(2, 49, "Babsan", 'f', { "female" }, interests3);
+	SECTION("Test menu infoABoutPerson()")
+	{
+		menu.infoAboutPerson(person3);
+	}
+	SECTION("Test menu add person")
+	{
+		//menu.newBoy();
+		
+		//menu.infoAboutPerson(*menu.getDate().getBoysList().getList().begin());
+	}
+	//system("pause");
+
 }
