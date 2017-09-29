d39240352a42d29f004f34d4e47d0b13f48070e9
BoboLillqvist <bobo.lillqvist@gmail.com>
2017-09-29 14:13:01 +0100

    Revert "P2 Lägg till test för infoAbout person"
    This reverts commit f69ed0244909f1101105049aa3228186989c8704.

--- projekt/dejt/dejt/Menu.h
+++ projekt/dejt/dejt/Menu.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "Date.h"
-#include <iomanip>
 
 //Kallar på date metoderna
 class Menu
@@ -59,7 +58,7 @@ public:
 	{
 		system("clear");
 		std::cout << "Main menu:"<<std::endl;
-		std::cout << "---------------" << std::endl;
+		std::cout << "---------" << std::endl;
 		std::cout << "1. Add new girl" << std::endl;
 		std::cout << "2. Add new boy" << std::endl;
 		std::cout << "3. Save to file" << std::endl;
@@ -71,7 +70,7 @@ public:
 	}
 	void newGirl()
 	{
-		newPerson('f');
+		std::cout << std::endl<<"Girl added!" << std::endl;
 		system("pause");
 		//New person
 		//Add person to date.girllist
@@ -79,108 +78,12 @@ public:
 
 	void newBoy()
 	{
-		newPerson('m');
+		std::cout << std::endl<<"Boy added!" << std::endl;
 		system("pause");
 		//New person
 		//Add person to date.girllist
 	}
-	void newPerson (char gender)
-	{
-		size_t id;
-		size_t age;
-		std::string name;
-		std::vector<std::string> preferences; 
-		std::string pref;
-		std::vector<std::string> interests;
-		std::string interest;
 
-		std::cout << "Vad är personens namn?" << std::endl;
-		getchar();
-		std::getline(std::cin, name);
-		std::cout << "Vad är personens ålder?" << std::endl;
-		std::cin >> age;
-		std::cout << "Vad är personens preferencer? (skriv vilka sorter genus hos partnern personen är intresserad av, en eller flera.)" << std::endl << "Tryck enter två gånger i rad för att avlusta" << std::endl;;
-		getchar();
-		while (std::getline(std::cin, pref))
-		{
-			if (pref == "")
-				break;
-			bool inList = false;
-			std::transform(pref.begin(), pref.end(), pref.begin(), tolower);
-			// Check if interest already in list
-			for (auto thisPreference : preferences)
-				if (thisPreference == pref)
-					inList = true;
-
-			if (!inList)
-				preferences.push_back(pref);
-		}
-
-		std::cout << "Vad är personens intressen?"<< std::endl << "Tryck enter två gånger i rad för att avlusta" << std::endl;;
-		getchar();
-		while (std::getline(std::cin, interest))
-		{
-			if (interest == "")
-				break;
-			bool inList = false;
-			std::transform(interest.begin(), interest.end(), interest.begin(), tolower);
-			// Check if interest already in list
-			for (auto thisInterest : interests)
-				if (thisInterest == interest)
-					inList = true;
-
-			if (!inList)
-				interests.push_back(interest);
-		}
-		id = date.numberOfPersons();
-		Person person(id, age, name, gender, preferences, interests);
-		if (gender == 'f')
-			date.getGirlsList().getList().push_front(person);
-		else if (gender=='m')
-			date.getBoysList().getList().push_front(person);
-		std::cout << "Info om personen som lades till" << std::endl << std::endl;
-		infoAboutPerson(person);
-	}
-	void infoAboutPerson(Person& person)
-	{
-		bool firstPref = true,firstInterest=true;
-		for(auto pref : person.getPreferences())
-		{
-			pref;
-		}
-		std::cout << std::endl <<
-			"Person: " << std::endl << "======================" << std::endl <<
-			"ID: " << std::setw(18) << person.getID() << std::endl <<
-			"Namn: " << std::setw(16) << person.getName() << std::endl <<
-			"Genus: " << std::setw(15) << person.getGender() << std::endl <<
-			"Preferencer: ";
-		for (auto pref : person.getPreferences())
-		{
-			if (firstPref)
-			{
-				std::cout << std::setw(9) << pref << std::endl;
-				firstPref = false;
-			}
-			else
-			{
-				std::cout << std::setw(22) << pref << std::endl;
-			}
-		}
-		std::cout <<std::endl<< "Intressen: ";
-		for (auto interest : person.getInterestTable().getList())
-		{
-			
-			if (firstInterest)
-			{
-				std::cout << std::setw(11) << interest << std::endl;
-				firstInterest = false;
-			}
-			else
-			{
-				std::cout << std::setw(22) << interest << std::endl;
-			}
-		}
-	}
 	void saveToFile()
 	{
 		std::cout << std::endl<< "File saved!" << std::endl;
@@ -200,9 +103,5 @@ public:
 		date.match();
 		std::cout << std::endl<< "Couples matched!" << std::endl;
 		system("pause");
-	}
-	Date& getDate()
-	{
-		return date;
 	}
 };
