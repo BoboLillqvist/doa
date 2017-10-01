2da42688022a41188542756995a1c94a22fdc13c
Tobias Lans <lanstobias@gmail.com>
2017-10-02 00:31:21 +0200

    P2 Lägg till kommentarer i Menu.h

--- projekt/dejt/dejt/Menu.h
+++ projekt/dejt/dejt/Menu.h
@@ -21,6 +21,7 @@ public:
 			display();
 			int switchCase = 0;
 			std::cin >> switchCase;
+
 			switch(switchCase)
 			{
 			case 1: 
@@ -44,7 +45,6 @@ public:
 			default:
 				system("pause");
 				break;
-
 			}
 			//Switch
 			//Case 1: Add new girl
@@ -55,6 +55,7 @@ public:
 			// Case 6: Quit
 		}
 	}
+
 	void display()
 	{
 		system("clear");
@@ -66,64 +67,74 @@ public:
 		std::cout << "4. Read from file" << std::endl;
 		std::cout << "5. Match couples" << std::endl;
 		std::cout << "6. Quit" << std::endl;
-		
-		//Vad som kommer upp på skärmen varje gång menysnurra körs
 	}
+
 	void newGirl()
 	{
 		newPerson('f');
 		system("pause");
-		//New person
-		//Add person to date.girllist
+		// New person
+		// Add person to date.girllist
 	}
 
 	void newBoy()
 	{
 		newPerson('m');
 		system("pause");
-		//New person
-		//Add person to date.girllist
+		// New person
+		// Add person to date.girllist
 	}
+
 	void newPerson (char gender)
 	{
-		size_t id;
-		size_t age;
-		std::string name;
-		std::vector<std::string> preferences; 
-		std::string pref;
-		std::vector<std::string> interests;
-		std::string interest;
+		size_t id, age;
+        std::string name, pref, interest;
+        std::vector<std::string> preferences, interests;
 
-		std::cout << "Vad är personens namn?" << std::endl;
-		getchar();
-		std::getline(std::cin, name);
-		std::cout << "Vad är personens ålder?" << std::endl;
-		std::cin >> age;
-		std::cout << "Vad är personens preferencer? (skriv vilka sorter genus hos partnern personen är intresserad av, en eller flera.)" << std::endl << "Tryck enter två gånger i rad för att avlusta" << std::endl;;
-		getchar();
+        // Assign person id based on number of persons in list
+		id = date.numberOfPersons();
+
+        // Get person info from user
+        std::cout << "Personens namn: "; getchar(); std::getline(std::cin, name);
+		std::cout << "Personens ålder: "; std::cin >> age;
+
+        // Get preferences from user
+		std::cout << "Vad är personens preferenser?"
+                  << "(skriv vilka sorter genus hos partnern personen är intresserad av, en eller flera.)"
+                  << std::endl << "Tryck enter två gånger i rad för att avsluta" << std::endl; getchar();
+
 		while (std::getline(std::cin, pref))
 		{
+			bool inList = false;
 			if (pref == "")
 				break;
-			bool inList = false;
+            
+            // Convert preference to lowercase
 			std::transform(pref.begin(), pref.end(), pref.begin(), tolower);
+
 			// Check if interest already in list
 			for (auto thisPreference : preferences)
 				if (thisPreference == pref)
 					inList = true;
 
+            // Add preference
 			if (!inList)
 				preferences.push_back(pref);
 		}
 
-		std::cout << "Vad är personens intressen?"<< std::endl << "Tryck enter två gånger i rad för att avlusta" << std::endl;;
-		getchar();
+        // Get interests from user
+		std::cout << "Vad är personens intressen?" << std::endl
+                  << "Tryck enter två gånger i rad för att avlusta" << std::endl; getchar();
+
 		while (std::getline(std::cin, interest))
 		{
+			bool inList = false;
 			if (interest == "")
 				break;
-			bool inList = false;
+
+            // Convert interest to lowercase
 			std::transform(interest.begin(), interest.end(), interest.begin(), tolower);
+
 			// Check if interest already in list
 			for (auto thisInterest : interests)
 				if (thisInterest == interest)
@@ -132,28 +143,35 @@ public:
 			if (!inList)
 				interests.push_back(interest);
 		}
-		id = date.numberOfPersons();
+
+        // Create person
 		Person person(id, age, name, gender, preferences, interests);
+
+        // Add to list
 		if (gender == 'f')
 			date.getGirlsList().getList().push_front(person);
 		else if (gender=='m')
 			date.getBoysList().getList().push_front(person);
+
+        // Print person
 		std::cout << "Info om personen som lades till" << std::endl << std::endl;
 		infoAboutPerson(person);
 	}
+
 	void infoAboutPerson(Person& person)
 	{
-		bool firstPref = true,firstInterest=true;
-		for(auto pref : person.getPreferences())
-		{
+		bool firstPref = true, firstInterest = true;
+		for (auto pref : person.getPreferences())
 			pref;
-		}
-		std::cout << std::endl <<
-			"Person: " << std::endl << "======================" << std::endl <<
-			"ID: " << std::setw(18) << person.getID() << std::endl <<
-			"Namn: " << std::setw(16) << person.getName() << std::endl <<
-			"Genus: " << std::setw(15) << person.getGender() << std::endl <<
-			"Preferencer: ";
+
+        // Print info
+        std::cout << std::endl
+                  << "Person: " << std::endl     << "======================" << std::endl
+                  << "ID: "     << std::setw(18) << person.getID()           << std::endl
+                  << "Namn: "   << std::setw(16) << person.getName()         << std::endl
+                  << "Genus: "  << std::setw(15) << person.getGender()       << std::endl;
+
+	    std::cout << "Preferencer: ";
 		for (auto pref : person.getPreferences())
 		{
 			if (firstPref)
@@ -166,10 +184,10 @@ public:
 				std::cout << std::setw(22) << pref << std::endl;
 			}
 		}
-		std::cout <<std::endl<< "Intressen: ";
+
+		std::cout << std::endl << "Intressen: ";
 		for (auto interest : person.getInterestTable().getList())
 		{
-			
 			if (firstInterest)
 			{
 				std::cout << std::setw(11) << interest << std::endl;
@@ -181,6 +199,7 @@ public:
 			}
 		}
 	}
+
 	void saveToFile()
 	{
 		std::cout << std::endl<< "File saved!" << std::endl;
@@ -201,6 +220,7 @@ public:
 		std::cout << std::endl<< "Couples matched!" << std::endl;
 		system("pause");
 	}
+
 	Date& getDate()
 	{
 		return date;
