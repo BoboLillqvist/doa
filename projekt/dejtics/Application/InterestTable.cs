90f92579b2ab9e9910309f1219030c268b1d5375
Tobias Lans <lanstobias@gmail.com>
2017-10-05 09:39:25 +0200

    InterestTable == operatoröverlagring

--- projekt/dejtics/Application/InterestTable.cs
+++ projekt/dejtics/Application/InterestTable.cs
@@ -44,5 +44,45 @@ namespace Application
 
             return commonCounter;
         }
+
+        // Auto generated
+        public override bool Equals(object obj)
+        {
+            var table = obj as InterestTable;
+            return table != null &&
+                   EqualityComparer<LinkedList<string>>.Default.Equals(List, table.List);
+        }
+
+        public override int GetHashCode()
+        {
+            return -2004394551 + EqualityComparer<LinkedList<string>>.Default.GetHashCode(List);
+        }
+
+        // Operator overloading
+        public static bool operator == (InterestTable lhs, InterestTable rhs)
+        {
+            // Flag
+            bool foundCommon = false;
+
+            foreach (var thisInterest in lhs.List)
+            {
+                foreach (var otherInterest in rhs.List)
+                    if (thisInterest == otherInterest)
+                        foundCommon = true;
+
+                if (!foundCommon)
+                    return false;
+
+                // Reset flag
+                foundCommon = false;
+            }
+
+            return false;
+        }
+
+        public static bool operator != (InterestTable rhs, InterestTable lhs)
+        {
+            return rhs == lhs;
+        }
     }
 }
