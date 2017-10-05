60fa181dda727f506a5824086ad667c671a4015e
Tobias Lans <lanstobias@gmail.com>
2017-10-05 09:49:40 +0200

    Skriv test för InterestTable

---
+++ projekt/dejtics/TestApplication/TestInterestTable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Application;
+
+namespace Application.Tests
+{
+    [TestClass]
+    public class TestInterestTable
+    {
+
+        [TestMethod]
+        public void Add()
+        {
+            InterestTable it = new InterestTable();
+
+            it.Add("SpoRT"); // +1
+            it.Add("bio");   // +1
+            it.Add("BIO");
+            it.Add("");
+            it.Add("Mat");   // +1
+            int addedInterests = 3;
+
+            Assert.AreEqual(addedInterests, it.NumberOfInterests());
+        }
+
+        [TestMethod()]
+        public void NumberOfCommonInterestsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
