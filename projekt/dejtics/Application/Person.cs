48f8d32c9df34ef543212a557327edc57bcc871e
Tobias Lans <lanstobias@gmail.com>
2017-10-05 09:40:07 +0200

    Lägg till medlemsvariabler i Person

--- projekt/dejtics/Application/Person.cs
+++ projekt/dejtics/Application/Person.cs
@@ -6,5 +6,12 @@ namespace Application
 {
     public class Person
     {
+        // Datamembers
+        public int ID { get; private set; }
+        public int Age { get; private set; }
+        public string Name { get; private set; }
+        public char Gender { get; private set; }
+
+        public InterestTable InterestsTable { get; private set; } = new InterestTable();
     }
 }
