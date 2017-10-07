129ca892738aacd2d47d5ff5171f6a5a8a8cda03
Tobias Lans <lanstobias@gmail.com>
2017-10-08 00:30:50 +0200

    Skapa PersonTest

---
+++ projekt/dejtics/TestApplication/PersonTest.cs
@@ -0,0 +1,12 @@
+﻿using Application;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+
+namespace Application.Tests
+{
+    [TestClass()]
+    public class PersonTest
+    {
+    }
+}
