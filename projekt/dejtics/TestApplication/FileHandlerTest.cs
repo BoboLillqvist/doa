9cc77c276652baa5d91e11a20b4210a51b317f25
Tobias Lans <lanstobias@gmail.com>
2017-10-08 00:27:29 +0200

    Skapa FileHandlerTest.Initialize()

--- projekt/dejtics/TestApplication/FileHandlerTest.cs
+++ projekt/dejtics/TestApplication/FileHandlerTest.cs
@@ -12,6 +12,28 @@ namespace Application.Tests
     [TestClass()]
     public class FileHandlerTest
     {
+        PersonList personList = new PersonList();
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            // Create Bob
+            List<string> interests1 = new List<string> {
+                "Sport", "Mat", "Fika", "Supa"
+            };
+            Person bob = new Person(0, 22, "Bob", 'm', interests1);
+
+            // Create Kenneth
+            List<string> interests2 = new List<string> {
+                "Handboll", "VVS-tekniker", "Iron Maiden", "Katter"
+            };
+            Person kenneth = new Person(1, 43, "Kenneth", 'm', interests2);
+
+            // Add to person list
+            personList.Add(bob);
+            personList.Add(kenneth);
+        }
+
         [TestMethod()]
         public void WriteTest()
         {
