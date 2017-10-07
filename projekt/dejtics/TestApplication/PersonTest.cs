cf09a1bbe08b6a74f4f39e846acd1ed770d2d840
Tobias Lans <lanstobias@gmail.com>
2017-10-08 00:31:13 +0200

    Skapa PersonTest.InterestsToStringTest()

--- projekt/dejtics/TestApplication/PersonTest.cs
+++ projekt/dejtics/TestApplication/PersonTest.cs
@@ -8,5 +8,31 @@ namespace Application.Tests
     [TestClass()]
     public class PersonTest
     {
+        [TestMethod()]
+        public void InterestsToStringTest()
+        {
+            List<string> interests1 = new List<string>();
+            interests1.Add("Sport");
+            interests1.Add("Mat");
+            interests1.Add("Fika");
+            interests1.Add("Supa");
+
+            // Create bob and add interests1
+            Person bob = new Person();
+            bob.AddToInterestTable(interests1);
+
+            // Create mange and add an empty list
+            List<string> interests2 = new List<string>();
+            Person mange = new Person(0, 39, "mange", 'm', interests2);
+
+            // Test bob
+            string bobExpectedStr = "supa,fika,mat,sport";
+            Assert.AreEqual(bobExpectedStr, bob.InterestsToString());
+
+            // Test mange
+            string expectedStr = "";
+            Assert.AreEqual(expectedStr, mange.InterestsToString());
+        }
+
     }
 }
