e8d2048cbf429349c29ad556f8db5068d3b68e79
Tobias Lans <lanstobias@gmail.com>
2017-10-09 12:58:00 +0200

    Fixa bugg i dejtics.Program

--- projekt/dejtics/dejtics/Program.cs
+++ projekt/dejtics/dejtics/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Application;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,9 +15,9 @@ namespace dejtics
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form());
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+            System.Windows.Forms.Application.Run(new Dejt());
         }
     }
 }
