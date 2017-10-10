2b7ca4e4c96dbd395e7a405f0c5967de2fb9b16d
BoboLillqvist <bobo.lillqvist@gmail.com>
2017-10-10 10:26:06 +0100

    Lägg till autogenererad designer för AddPerson

---
+++ projekt/dejtics/dejtics/FormAddPerson.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace dejtics
+{
+    partial class FormAddPerson
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AcceptBtn = new System.Windows.Forms.Button();
+            this.CancelBtn = new System.Windows.Forms.Button();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.InterestsTextBox = new System.Windows.Forms.TextBox();
+            this.GenderTextBox = new System.Windows.Forms.TextBox();
+            this.AgeTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // AcceptBtn
+            // 
+            this.AcceptBtn.Location = new System.Drawing.Point(77, 335);
+            this.AcceptBtn.Name = "AcceptBtn";
+            this.AcceptBtn.Size = new System.Drawing.Size(75, 23);
+            this.AcceptBtn.TabIndex = 5;
+            this.AcceptBtn.Text = "Accept";
+            this.AcceptBtn.UseVisualStyleBackColor = true;
+            this.AcceptBtn.Click += new System.EventHandler(this.AcceptBtn_Click);
+            // 
+            // CancelBtn
+            // 
+            this.CancelBtn.Location = new System.Drawing.Point(186, 335);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.CancelBtn.TabIndex = 6;
+            this.CancelBtn.Text = "Cancel";
+            this.CancelBtn.UseVisualStyleBackColor = true;
+            this.CancelBtn.Click += new System.EventHandler(this.CancelBtn_Click);
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(77, 54);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(184, 20);
+            this.NameTextBox.TabIndex = 1;
+            // 
+            // InterestsTextBox
+            // 
+            this.InterestsTextBox.Location = new System.Drawing.Point(77, 197);
+            this.InterestsTextBox.Multiline = true;
+            this.InterestsTextBox.Name = "InterestsTextBox";
+            this.InterestsTextBox.Size = new System.Drawing.Size(184, 100);
+            this.InterestsTextBox.TabIndex = 4;
+            // 
+            // GenderTextBox
+            // 
+            this.GenderTextBox.Location = new System.Drawing.Point(77, 147);
+            this.GenderTextBox.Name = "GenderTextBox";
+            this.GenderTextBox.Size = new System.Drawing.Size(184, 20);
+            this.GenderTextBox.TabIndex = 3;
+            // 
+            // AgeTextBox
+            // 
+            this.AgeTextBox.Location = new System.Drawing.Point(77, 94);
+            this.AgeTextBox.Name = "AgeTextBox";
+            this.AgeTextBox.Size = new System.Drawing.Size(184, 20);
+            this.AgeTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Age:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Gender:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Interests";
+            // 
+            // FormAddPerson
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 371);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AgeTextBox);
+            this.Controls.Add(this.GenderTextBox);
+            this.Controls.Add(this.InterestsTextBox);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.CancelBtn);
+            this.Controls.Add(this.AcceptBtn);
+            this.Name = "FormAddPerson";
+            this.Text = "FormAddPerson";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AcceptBtn;
+        private System.Windows.Forms.Button CancelBtn;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.TextBox InterestsTextBox;
+        private System.Windows.Forms.TextBox GenderTextBox;
+        private System.Windows.Forms.TextBox AgeTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
